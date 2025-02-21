272c292531f46505d96d026b0d59e3949a028a0b
Noorul-Ayn <h.bello@alustudent.com>
2025-02-21 18:50:19 +0200

    Add files via upload

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,28 +7,28 @@ import requests
 
 
 def top_ten(subreddit):
-    """
-    A function that fetches and prints the titles
-    of the top ten hot posts from a subreddit.
-    """
+        """
+            A function that fetches and prints the titles
+                of the top ten hot posts from a subreddit.
+                    """
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(
-        url,
-        params={"after": None},
-        allow_redirects=False,
-        headers=headers,
-    )
+                        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+                            headers = {"User-Agent": "Mozilla/5.0"}
+                                response = requests.get(
+                                                url,
+                                                        params={"after": None},
+                                                                allow_redirects=False,
+                                                                        headers=headers,
+                                                                            )
 
-    if response.status_code != 200:
-        print(None)
-        return
+                                    if response.status_code != 200:
+                                                print(None)
+                                                        return
 
-    jsonData = response.json()
-    data = jsonData["data"]["children"]
-    for post in data:
-        print(post.get("data", {}).get("title"))
+                                                        jsonData = response.json()
+                                                            data = jsonData["data"]["children"]
+                                                                for post in data:
+                                                                            print(post.get("data", {}).get("title"))
 
 
-# top_ten("programming")
+                                                                            # top_ten("programming")
